12f1c683f41367a8b6f929b77bff8dd1c6d48d83
Chetan Dhangar <chetansdhangar@gmail.com>
2020-12-23 18:45:53 +0530

    removing task fron the todo list done

--- src/App.js
+++ src/App.js
@@ -19,23 +19,32 @@ function App() {
     setUserInput("")
   }
 
+  function deleteTask(index){
+    const taskvalueDelete = [...tasks]
+     taskvalueDelete.splice(index, 1)
+     setTasks(taskvalueDelete)
+  
+    
+  }
+
   return (
     <div className="App">
 
       <h1>ToDo List</h1>
+      <h2>Current Todo's to Do : {tasks.length}</h2>
      
       <div>
      
-        {tasks.map(task=>(
+        {tasks.map((task, index)=>(
           <ul> 
-            <li key={task.id}>
-              {task.value}
+            <li key={index}>
+              {task.value} <button onClick={()=>deleteTask(index)}>Delete Task</button>
             </li>
+            
           </ul>
         ))}
-
-<button onClick={addTask}>Add a Taks to list</button>
-<input onChange={handleChange}  value={userInput}/>
+        <input onChange={handleChange}  value={userInput}/>
+        <button onClick={addTask}>Add a Taks to list</button>
       </div>
      
      
